40f642577f9193d45cc033b51873f12bb918b4df
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-06-08 22:29:39 -0500

    Initial Commit

---
+++ CalculatorApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorApp
+//
+//  Created by Dinesh Danda on 6/8/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
